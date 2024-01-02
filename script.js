83ba6dd35a96db62404a727bd6c552a3a8d1f685
pea06 <jowangyu75@dgsw.hs.kr>
2024-01-02 23:14:45 +0900

    publish

---
+++ script.js
@@ -0,0 +1,19 @@
+const wrap = document.getElementsByClassName('wrap')[0]; 
+    const container = document.getElementsByClassName('container');
+    let page = 0;
+    const lastPage = container.length - 1;
+
+    window.addEventListener('wheel', (e) => {
+        e.preventDefault();
+        if (e.deltaY > 0) {
+            page++;
+        } else if (e.deltaY < 0) {
+            page--;
+        }
+        if (page < 0) {
+            page = 0;
+        } else if (page > lastPage) {
+            page = lastPage;
+        }
+        wrap.style.top = page * -100 + 'vh';
+    }, { passive: false });
